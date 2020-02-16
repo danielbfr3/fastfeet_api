f5c24bb83e26fa51340a2faa01a4f7215b43a6b8
Daniel Freire <danielbfr3@gmail.com>
2020-02-16 02:29:08 -0300

    Configuring mailtrap.io

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: '',
-  port: '',
+  host: 'smtp.mailtrap.io',
+  port: '2525',
   secure: false,
   auth: {
-    user: '',
-    password: '',
+    user: '8dd286c32a244b',
+    pass: 'e03ac2cf3fc763',
   },
   default: {
     from: 'GoBarber Team <noreply@gobarber.com>',
