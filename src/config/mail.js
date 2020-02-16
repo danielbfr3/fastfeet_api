c34724f23b047acb16d07d52fe74b70488b06b5f
Daniel Freire <danielbfr3@gmail.com>
2020-02-16 01:28:20 -0300

    Adding config file

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: '',
+  port: '',
+  secure: false,
+  auth: {
+    user: '',
+    password: '',
+  },
+  default: {
+    from: 'GoBarber Team <noreply@gobarber.com>',
+  },
+};
